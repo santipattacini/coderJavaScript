690dfe1c9e9feb4829d30d38b69c61698fd24b07
Santiago Pattacini <santiagopattacini@gmail.com>
2022-09-06 22:39:03 -0300

    incorporacion de librerias

--- script.js
+++ script.js
@@ -39,28 +39,43 @@ const seguro5 = new Seguro("volkswagen", 1500000, 3000)
 
 const seguros = [seguro1, seguro2, seguro3, seguro4, seguro5]
 const segurosMarcas = seguros.map(seguro => seguro.marca)
+// ordenamos alfabeticamente
+segurosMarcas.sort()
 
 // consulto por el valor que el usuario ingreso en el input
 const form = document.getElementById("marcaForm")
+const select = document.getElementById("formSelect")
 const cotizacion = document.getElementById("cotizacion")
+
+console.log(segurosMarcas)
+
+// funcion para implementar valores en la lista
+for(var i = 0; i < segurosMarcas.length; i++){
+    var seg = segurosMarcas[i]
+    var list = document.createElement("option")
+    list.textContent = seg
+    list.value = seg
+    select.appendChild(list) 
+} 
+
 form.addEventListener("submit", (e) => {
     e.preventDefault()
-    const marca = document.getElementById("idMarca").value
+    const marca = document.getElementById("formSelect").value
     if (seguros.some(seguros => seguros.marca == marca) == true){
         const indice = segurosMarcas.indexOf(marca)
         cotizacion.classList.remove("colorRed")
         cotizacion.innerText = `La marca ${marca} está cotizada en $${seguros[indice].cotizacion} y el seguro tiene un valor de $${seguros[indice].valor}`
     } else {
-        cotizacion.innerText = `No cotizamos seguros para la marca ${marca}`
-        cotizacion.classList.add("colorRed")
+        // cotizacion.innerText = `No cotizamos seguros para la marca ${marca}`
+        // cotizacion.classList.add("colorRed")
+        Swal.fire({
+            title: 'No seleccionaste ninguna marca!',
+            text: 'Debes seleccionar alguna para que podamos cotizar',
+            color: 'black',
+            imageUrl: 'img/meme-gato.jpg',
+            imageWidth: 300,
+            background: '#e29a9a',
+          })
     }
 })
 
-// listado de marcas disponibles para asegurar
-const marcasCotizadas = document.getElementById("marcasCotizadas")
-marcasCotizadas.addEventListener("click", () => {
-    listado.innerText = ""
-    segurosMarcas.forEach((elemento) => {
-        listado.innerText += `- ${elemento} \n`
-    })
-})
